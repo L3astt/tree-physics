b53562a1f97defe879fcb8211e2bfbcdabcd4198
KyanBirb <97422254+KyanBirb@users.noreply.github.com>
2026-05-07 18:35:32 -0500

    Make bee nests break on impact

---
+++ src/main/java/com/farcr/treephysics/mixin/collision_effects/BeehiveBlockCallbackMixin.java
@@ -0,0 +1,22 @@
+package com.farcr.treephysics.mixin.collision_effects;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import dev.ryanhcode.sable.physics.callback.BeehiveBlockCallback;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.entity.BeehiveBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(BeehiveBlockCallback.class)
+public class BeehiveBlockCallbackMixin {
+
+    @WrapOperation(method = "onHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/BeehiveBlockEntity;emptyAllLivingFromHive(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/block/entity/BeehiveBlockEntity$BeeReleaseStatus;)V"))
+    private void treephysics$onHit(BeehiveBlockEntity instance, Player player, BlockState state, BeehiveBlockEntity.BeeReleaseStatus releaseStatus, Operation<Void> original, @Local(argsOnly = true) ServerLevel level) {
+        level.destroyBlock(instance.getBlockPos(), true);
+        original.call(instance, player, state, releaseStatus);
+    }
+}
